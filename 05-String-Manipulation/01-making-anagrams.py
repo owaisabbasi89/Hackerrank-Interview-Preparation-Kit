ae64400e9817fc61398105ee139493c9a1d50d75
Owais Abbasi <owais.abbasi9@gmail.com>
2024-03-20 02:49:31 +0500

    practiced Dicts, Sorting, String Manipulation

---
+++ 05-String-Manipulation/01-making-anagrams.py
@@ -0,0 +1,96 @@
+'''
+
+A student is taking a cryptography class and has found anagrams to be very useful. Two strings are anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+The student decides on an encryption scheme that involves two large strings. The encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Determine this number.
+
+Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+
+Example
+
+
+Delete  from  and  from  so that the remaining strings are  and  which are anagrams. This takes  character deletions.
+
+Function Description
+
+Complete the makeAnagram function in the editor below.
+
+makeAnagram has the following parameter(s):
+
+string a: a string
+string b: another string
+Returns
+
+int: the minimum total characters that must be deleted
+Input Format
+
+The first line contains a single string, .
+The second line contains a single string, .
+
+Constraints
+
+The strings  and  consist of lowercase English alphabetic letters, ascii[a-z].
+Sample Input
+
+cde
+abc
+Sample Output
+
+4
+Explanation
+
+Delete the following characters from the strings make them anagrams:
+
+Remove d and e from cde to get c.
+Remove a and b from abc to get c.
+It takes  deletions to make both strings anagrams.
+
+'''
+
+#
+# Complete the 'makeAnagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING a
+#  2. STRING b
+#
+from collections import Counter
+def makeAnagram(a, b):
+    # Write your code here
+    chrCount = 0
+    x = Counter(a)
+    y = Counter(b)
+    chrNotInXbutY = list(set(b) - (set(a).intersection(set(b))))
+    for i in x.keys():
+        if i in y.keys():
+            if x[i] == y[i]:
+                continue
+            else:
+                chrCount += abs(x[i] - y[i])
+        else:
+            chrCount += x[i]
+
+    for i in chrNotInXbutY:
+        if i in x.keys():
+            if y[i] == x[i]:
+                continue
+            else:
+                chrCount += abs(y[i] - x[i])
+        else:
+            chrCount += y[i]
+            
+    
+    
+    return chrCount
+
+if __name__ == '__main__':
+
+
+    a = input()
+
+    b = input()
+
+    res = makeAnagram(a, b)
+
+    print(res)
--- testing.py
+++ testing.py
@@ -1,26 +1,120 @@
 
-def hourglassSum(arr):
-    # Write your code here
-    summ = []
-    flat = []
-    for i in arr:
-        for j in i:
-            flat.append(j)
+# def hourglassSum(arr):
+#     # Write your code here
+#     summ = []
+#     flat = []
+#     for i in arr:
+#         for j in i:
+#             flat.append(j)
     
-    for i, item in enumerate(flat):
-        if i >= 24 or i ==4 or i==5 or i==10 or i==11 or i==16 or i==17 or i==22 or i==23:
-            continue
+#     for i, item in enumerate(flat):
+#         if i >= 24 or i ==4 or i==5 or i==10 or i==11 or i==16 or i==17 or i==22 or i==23:
+#             continue
+#         else:
+#             hourglass_sum = sum(flat[i:i+3]) + sum(flat[i+7:i+8]) + sum(flat[i+12:i+15])
+#             summ.append(hourglass_sum)
+#     return max(summ)
+
+
+# arr = []
+
+# for _ in range(6):
+#     arr.append(list(map(int, input().rstrip().split())))
+
+# result = hourglassSum(arr)
+
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+# def minimumBribes(q):
+#     # Write your code here
+#     bribeCount = 0
+#     bc = []
+#     flag = True
+#     originalPos = list(range(1, (len(q)+1)))
+#     for i in q:
+#         for j in originalPos:
+#             diff = i - j
+#             if diff <= 0:
+#                 continue
+#             elif diff >= 3:
+#                 flag = False
+#                 # break
+#             else:
+#                 bribeCount += diff
+#                 bc.append(diff)
+                
+#     # if flag == False:
+#     #     print('Too chaotic')
+#     # else:
+#     #     print(bribeCount)
+#     print(bc)
+#     print(bribeCount)
+                
+        
+# q = [2, 1, 5, 3, 4]        
+# minimumBribes(q)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from collections import Counter
+def makeAnagram(a, b):
+    # Write your code here
+    chrCount = 0
+    x = Counter(a)
+    y = Counter(b)
+    for i in x.keys():
+        if i in y.keys():
+            if x[i] == y[i]:
+                continue
+            else:
+                chrCount += abs(x[i] - y[i])
         else:
-            hourglass_sum = sum(flat[i:i+3]) + sum(flat[i+7:i+8]) + sum(flat[i+12:i+15])
-            summ.append(hourglass_sum)
-    return max(summ)
+            chrCount += x[i]
+
+    for i in y.keys():
+        if i in x.keys():
+            if y[i] == x[i]:
+                continue
+            else:
+                chrCount += abs(y[i] - x[i])
+        else:
+            chrCount += y[i]
+            
+    
+    
+    return x,y
+
+if __name__ == '__main__':
 
 
-arr = []
+    a = input()
 
-for _ in range(6):
-    arr.append(list(map(int, input().rstrip().split())))
+    b = input()
 
-result = hourglassSum(arr)
+    res = makeAnagram(a, b)
 
-print(result)
+    print(res)
